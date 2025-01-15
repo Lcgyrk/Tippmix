1a2915d86dc5c41ad7c451a905d8c93c5ce3d5ab
p4likas51 <schmitzhofer.pal@students.jedlik.eu>
2025-01-15 14:03:49 +0100

    #8 alap config

---
+++ public/main.js
@@ -0,0 +1,17 @@
+"use strict";
+/*
+https://www.typescriptlang.org/docs/handbook/intro.html
+https://www.youtube.com/watch?v=asC7jxGW45Q&list=PL6VA7Z5zO0dpy0uvNllS5-zyNNl5otN5f&ab_channel=K%C3%B3db%C3%A1zis
+
+// telepítés
+npm i -g typescript
+
+npx tsc -v
+
+// új ts fejlesztői környezet inicializálás
+npx tsc --init
+
+// tsc futtatás
+npx tsc -w
+*/
+console.log('ok');
