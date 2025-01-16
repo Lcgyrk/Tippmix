66b5915da69d883d06256949e126e2b25f988456
[Pap László] <[pap.laszlo@students.jedlik.eu]>
2025-01-16 10:46:32 +0100

    #3 jsonnnel még szopok do not worry

--- src/login.ts
+++ src/login.ts
@@ -2,16 +2,32 @@ let registrationName = document.getElementById("name") as HTMLInputElement;
 let registrationEmail = document.getElementById("email") as HTMLInputElement;
 let registrationPassword = document.getElementById("password") as HTMLInputElement;
 
+interface user {
+    name: string;
+    email: string;
+    password: string;
+}
+
+let users: user[] = [];
+
 function Registration(){
     let name = registrationName.value;
     let email = registrationEmail.value;
     let password = registrationPassword.value;
-    console.log(name, email, password);
+
+    if (users.find(user => user.name === name)) {
+        alert("Ez a felhasználónév már foglalt!");
+        return;
+    }
+
     let user = {
         name: name,
         email: email,
         password: password
     }
+    users.push(user);
+    console.log(users);
+    
 }
 
 document.getElementById("register")!.addEventListener("click", Registration);
