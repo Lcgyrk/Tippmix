1eae9467282ecc6899fe833e4ba3425853912985
p4likas51 <schmitzhofer.pal@students.jedlik.eu>
2025-01-22 13:57:36 +0100

    valami legyen

--- public/login.js
+++ public/login.js
@@ -2,15 +2,21 @@
 let registrationName = document.getElementById("name");
 let registrationEmail = document.getElementById("email");
 let registrationPassword = document.getElementById("password");
+let users = [];
 function Registration() {
     let name = registrationName.value;
     let email = registrationEmail.value;
     let password = registrationPassword.value;
-    console.log(name, email, password);
+    if (users.find(user => user.name === name)) {
+        alert("Ez a felhasználónév már foglalt!");
+        return;
+    }
     let user = {
         name: name,
         email: email,
         password: password
     };
+    users.push(user);
+    console.log(users);
 }
 document.getElementById("register").addEventListener("click", Registration);
