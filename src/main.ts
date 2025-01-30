3748a7ee07ba677982d7396236ee211f36dd6f38
p4likas51 <schmitzhofer.pal@students.jedlik.eu>
2025-01-30 11:15:49 +0100

    #7 #2 sok funkcio hozzaadas

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,46 @@
 import { FetchBets } from "./readFile.js";
 
-async function GetTenMatch() {
+let allOdds: number[] = [];
+const footballBetting = document.getElementById('football-betting');
+footballBetting!.addEventListener('click', () => {
+        if (!footballBetting?.classList.contains("loaded")) {
+            footballBetting?.classList.add('loaded');
+            GetMatches(5, 'soccer');
+        }
+});
+const basketballBetting = document.getElementById('basketball-betting');
+basketballBetting!.addEventListener('click', () => {
+        if (!basketballBetting?.classList.contains("loaded")) {
+            basketballBetting?.classList.add('loaded');
+            GetMatches(5, 'basketball');
+        }
+});
+const cricketBetting = document.getElementById('cricket-betting');
+cricketBetting!.addEventListener('click', () => {
+        if (!cricketBetting?.classList.contains("loaded")) {
+            cricketBetting?.classList.add('loaded');
+            GetMatches(5, 'cricket');
+        }
+});
+const americanFootballBetting = document.getElementById('americanFootball-betting');
+americanFootballBetting!.addEventListener('click', () => {
+        if (!americanFootballBetting?.classList.contains("loaded")) {
+            americanFootballBetting?.classList.add('loaded');
+            GetMatches(5, 'american football');
+        }
+});
+const tennisBetting = document.getElementById('tennis-betting');
+tennisBetting!.addEventListener('click', () => {
+        if (!tennisBetting?.classList.contains("loaded")) {
+            tennisBetting?.classList.add('loaded');
+            GetMatches(5, 'tennis');
+        }
+});
+async function GetMatches(numberOfMatches: number, typeOfSport: string) {
     const data = await FetchBets();
-    const matches = getRandomMatches(data, 10);
+    const matches = getRandomMatches(data, numberOfMatches, typeOfSport);
     const matchesContainer = document.getElementById("matches-container");
+    matchesContainer!.innerHTML = ''
     matches.forEach((match) => {
         matchesContainer!.innerHTML += `
         <div class="card-body">
@@ -15,7 +52,7 @@ async function GetTenMatch() {
             <div class="d-flex justify-content-around mt-3">
                 <button id="${match.id}-${
             match.homeTeam
-        }" class="btn btn-outline-primary">${match.homeOdds}</button>
+        }" class="btn btn-outline-primary match">${match.homeOdds}</button>
                 ${
                     match.drawOdds !== null
                         ? `<button id="${match.id}-draw" class="btn btn-outline-secondary">${match.drawOdds}</button>`
@@ -32,13 +69,29 @@ async function GetTenMatch() {
             <button class="btn btn-success w-100 mt-3">Place Bet</button>
         </div>
     `;
-    });
+});
+const matchButtons = document.querySelectorAll<HTMLButtonElement>('.match');
+
+matchButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        const target = event.target as HTMLButtonElement;
+        if (button.classList.contains("btn-primary")) {
+            allOdds.splice(allOdds.indexOf(Number(target.innerText)), 1);
+            button.classList.remove("btn-primary");
+            button.classList.add("btn-outline-primary");
+        } else {
+            allOdds.push(Number(target.innerText));
+            button.classList.remove("btn-outline-primary");
+            button.classList.add("btn-primary");
+        }
+        console.log(allOdds);
+    })
+})
 }
 
-function getRandomMatches<T>(array: T[], count: number): T[] {
-    return array.sort(() => Math.random() - 0.5).slice(0, count);
+function getRandomMatches<T extends {sport: string}>(array: T[], count: number, typeOfSport: string): T[] {
+    const filteredArray = array.filter(item => (item.sport).toLowerCase() == (typeOfSport).toLocaleLowerCase());
+    return filteredArray.sort(() => Math.random() - 0.5).slice(0, count);
 }
 
-GetTenMatch();
-
-document.querySelectorAll("button");
+console.log(allOdds);
