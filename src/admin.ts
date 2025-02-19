4bfe9a81f506fbf68d60b545c2aff3d27e5e7ba5
[Pap László] <[pap.laszlo@students.jedlik.eu]>
2025-02-19 12:18:47 +0100

    #5 elkezdve

--- src/admin.ts
+++ src/admin.ts
@@ -7,14 +7,20 @@ interface user {
 }
 
 let users1: user[] = [];
-let usersloaded1 = false;
-
-
-
-
+let usersloadedForAdmin = false;
 
 const seeUsers = document.getElementById("userControl") as HTMLButtonElement;
 seeUsers.addEventListener("click", () => {
     const userDiv = document.getElementById("usersList");
     userDiv!.innerHTML = "";
 });
+
+const seeMatches = document.getElementById("matchControl") as HTMLInputElement;
+seeMatches.addEventListener("click", () => {
+    const matchesDiv = document.getElementById("matchesList");
+    matchesDiv!.innerHTML = "";
+});
+
+async function showUsers() {
+    
+}
