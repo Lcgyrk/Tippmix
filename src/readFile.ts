d50cbf6670fd354f2b84ef6e15e5e020eff2c193
p4likas51 <schmitzhofer.pal@students.jedlik.eu>
2025-02-20 10:31:32 +0100

    #3 login ujratervezes

--- src/readFile.ts
+++ src/readFile.ts
@@ -2,6 +2,7 @@ export interface User {
     id: number;
     name: string;
     email: string;
+    password: string
     credits: number;
 }
 
@@ -14,14 +15,34 @@ export interface Bet {
     awayOdds: number;
     drawOdds: number | null;
 }
+
+let users: User[] = [];
+let usersloaded = false;
+
 export async function FetchBets(): Promise<Bet[]> {
     const response = await fetch("http://localhost:3000/bets");
     if (!response.ok) throw new Error(response.statusText);
     return await response.json();
 }
 
-export async function FetchCurrentMatches(): Promise<Bet[]> {
-    const response = await fetch("http://localhost:3000/currentMatches");
-    if (!response.ok) throw new Error(response.statusText);
-    return await response.json();
+async function FetchUsers(){
+
+    if (usersloaded) return;
+
+    try {
+        const response = await fetch("http://localhost:3000/users");
+        const data = await response.json();
+
+        users = data.map((user: any) => ({
+            ...user,
+            id: Number(user.id)
+        }));
+
+        usersloaded = true;
+        console.log("Users loaded:", users);
+    } catch (error) {
+        console.error("Failed to fetch users:", error);
+    }
 }
+FetchUsers();
+export {users}
